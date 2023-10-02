f8c720f76ebcd63736a764f762f8c19574291f65
Minh Triet Pham <>
2023-10-02 17:36:13 +0700

    load welcome screen

---
+++ SportNews0210/SportNews0210/Slide.swift
@@ -0,0 +1,18 @@
+//
+//  Slide.swift
+//  SportNews0210
+//
+//  Created by Minh Triet Pham on 02/10/2023.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    
+}
