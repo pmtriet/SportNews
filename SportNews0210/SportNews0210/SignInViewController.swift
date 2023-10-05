8a05f0d8ce7f1f078228666b8b399e8af3e3de60
Minh Triet Pham <>
2023-10-05 15:08:59 +0700

    kiem tra contrainst giup em voi anh oi

--- SportNews0210/SportNews0210/SignInViewController.swift
+++ SportNews0210/SportNews0210/SignInViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var createAccountLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        let signUp = "or Create Account"
+        let attributedString = NSMutableAttributedString(string: signUp)
+        let range = (attributedString.string as NSString).range(of: "Create Account")
+        attributedString.addAttributes([.underlineStyle: NSUnderlineStyle.single.rawValue], range: range)
+        createAccountLabel.attributedText = attributedString
+        
+        
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(labelTapped))
+        createAccountLabel.addGestureRecognizer(tapGestureRecognizer)
+        createAccountLabel.isUserInteractionEnabled = true
+        
     }
     
-
+    @objc func labelTapped() {
+        let signUp = SignUpViewController()
+        navigationController?.pushViewController(signUp, animated: true)
+       }
 }
---
+++ SportNews0210/SportNews0210/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  SportNews0210
+//
+//  Created by Minh Triet Pham on 05/10/2023.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+
+}
